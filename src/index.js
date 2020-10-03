e791123462c7a277d4a94ec5d0b2292a298c0579
AnnSverchok23 <annasverchkova23@gmail.com>
2020-10-03 23:59:14 +0300

    docs:update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
+   return Math.min.apply(null, array);
   return 0;
 }
 
 exports.max = function max (array) {
+    return Math.max.apply(null, array);
   return 0;
 }
 
 exports.avg = function avg (array) {
+    return array.reduce((sum, current) => sum+current, 0) /array.length ;
   return 0;
 }
