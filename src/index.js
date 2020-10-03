71145225db97bccccb98dbb00f85a66ca2ae2aa7
AnnSverchok23 <annasverchkova23@gmail.com>
2020-10-04 00:29:10 +0300

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 
 exports.min = function min (array) {
+if( Array.isArray(array) && array.length !== 0){
    return Math.min.apply(null, array);
+}
   return 0;
+   
 }
 
 exports.max = function max (array) {
+    if( Array.isArray(array) && array.length !== 0){
     return Math.max.apply(null, array);
+    }
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if( Array.isArray(array) && array.length !== 0){
     return array.reduce((sum, current) => sum+current, 0) /array.length ;
+    }
   return 0;
 }
